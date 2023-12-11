3058b47387bd0c08cb335e2833cef8d183c5f62d
f0rc <brickmp4@gmail.com>
2023-12-10 23:01:19 -0600

    init

---
+++ src/server/db/index.ts
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+import { env } from "~/env";
+import * as schema from "./schema";
+
+export const db = drizzle(
+  new Pool({
+    connectionString: env.DATABASE_URL,
+  }),
+  { schema },
+);
